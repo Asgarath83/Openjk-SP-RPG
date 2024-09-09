9b67fff4879148fda465fb2faf08344659e08ec6
Asgarath83 <pisanod5@gmail.com>
2024-09-09 11:30:54 +0200

    Add files via upload

---
+++ qcommon/game_version.h
@@ -0,0 +1,78 @@
+/*
+===========================================================================
+Copyright (C) 2000 - 2013, Raven Software, Inc.
+Copyright (C) 2001 - 2013, Activision, Inc.
+Copyright (C) 2013 - 2015, OpenJK contributors
+
+This file is part of the OpenJK source code.
+
+OpenJK is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see <http://www.gnu.org/licenses/>.
+===========================================================================
+*/
+
+#ifndef _GAME_VERSION_H
+#define _GAME_VERSION_H
+
+#define _STR(x) #x
+#define STR(x) _STR(x)
+
+#include "../win32/AutoVersion.h"
+//#include "../git.h"
+
+#define ASTROFANTASY_VERSION "1.0"
+// Current version of the Single player game
+#ifdef _DEBUG
+	#define JK_VERSION "" GIT_BRANCH " / " GIT_HASH " (debug)"
+	#define ASTROFANTASY_VERSION_SUFFIX "a"
+	
+#elif defined FINAL_BUILD
+	#define ASTROFANTASY_VERSION "" GIT_BRANCH " / " GIT_HASH ""
+	#define ASTROFANTASY_VERSION_SUFFIX ""
+	
+#else
+	#define ASTROFANTASY_VERSION "" GIT_BRANCH " / " GIT_HASH " (internal)"
+	#define ASTROFANTASY_VERSION_SUFFIX "r"
+#endif
+
+#define	GAMEVERSION	"Astrofantasy v" ASTROFANTASY_VERSION ASTROFANTASY_VERSION_SUFFIX
+
+#endif
+
+//end
+
+
+
+
+#define JKG_VERSION "1.3.18"
+// Current version of the multi player game
+#ifdef _DEBUG
+	#define JK_VERSION "" GIT_BRANCH " / " GIT_HASH " (debug)"
+	#define JKG_VERSION_SUFFIX "a"
+	
+#elif defined FINAL_BUILD
+	#define JK_VERSION "" GIT_BRANCH " / " GIT_HASH ""
+	#define JKG_VERSION_SUFFIX ""
+	
+#else
+	#define JK_VERSION "" GIT_BRANCH " / " GIT_HASH " (internal)"
+	#define JKG_VERSION_SUFFIX "r"
+#endif
+
+#define	GAMEVERSION	"Jedi Knight Galaxies v" JKG_VERSION JKG_VERSION_SUFFIX
+
+#endif
+
+//end
+
+
+
